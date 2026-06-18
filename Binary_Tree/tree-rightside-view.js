045913b6789b083feea0522d5af115bdb01d4435
Zuwairiya-1147 <sulaikzuwairiya@gmail.com>
2026-06-18 19:13:48 +0530

    Binary Tree and Binary Search Tree

---
+++ Binary_Tree/tree-rightside-view.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<Integer> arr = new LinkedList<>();
+        if(root == null) return arr;
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i=0; i<size; i++) {
+                 TreeNode curr = q.poll();
+                if(i == size-1) arr.add(curr.val);
+                if(curr.left!=null) q.offer(curr.left);
+                if(curr.right!=null) q.offer(curr.right);
+            }
+        }
+        return arr;
+    }
+}
